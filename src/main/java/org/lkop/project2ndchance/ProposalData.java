2193aa0ec485903071d7fd1628b97122a0a805c3
Lkop <37883287+Lkop@users.noreply.github.com>
2019-07-24 02:27:19 +0300

    Adding some fields...

--- src/main/java/org/lkop/project2ndchance/ProposalData.java
+++ src/main/java/org/lkop/project2ndchance/ProposalData.java
@@ -2,18 +2,28 @@ package org.lkop.project2ndchance;
 
 public class ProposalData {
     
-    private int id;
+    private int id=0;
     private String name;
     private String lastname;
     private String email;
+    private int semester;
+    
+    //mentor's data
+    private String mname;
+    private String mlastname;
+    private String memail;
+    
     
     public ProposalData(){
 
-        this.name = null;
-        this.lastname = null;
-        this.email = null; 
+        this.id++;
     }
     
+    public int getId(){
+    
+        return this.id;
+    }
+        
     public void setName(String name){
     
         this.name = name;
@@ -29,8 +39,59 @@ public class ProposalData {
         this.lastname = lastname;
     }
     
+    public String getLastname(){
+    
+        return this.lastname;
+    }
+        
     public void setEmail(String email){
     
         this.email = email;
+    }
+    
+    public String getEmail(){
+    
+        return this.email;
+    }
+    
+    public void setSemester(int semester){
+    
+        this.semester = semester;
+    }
+    
+    public int getSemester(){
+    
+        return this.semester;
+    }
+    
+    //mentor's methods
+    public void setMName(String name){
+    
+        this.mname = name;
+    }
+    
+    public String getMName(){
+    
+        return this.mname;
+    }
+    
+    public void setMLastname(String lastname){
+    
+        this.mlastname = lastname;
+    }
+    
+    public String getMLastname(){
+    
+        return this.mlastname;
+    }
+    
+    public void setMEmail(String email){
+    
+        this.memail = email;
+    }
+    
+    public String getMEmail(){
+    
+        return this.memail;
     }
 }
