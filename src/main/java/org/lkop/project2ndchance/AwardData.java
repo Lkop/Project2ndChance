8ab114cfc0236e6244b81e6fd3fb29641562b07c
Lkop <37883287+Lkop@users.noreply.github.com>
2019-07-29 16:51:41 +0300

    Awards Fields

---
+++ src/main/java/org/lkop/project2ndchance/AwardData.java
@@ -0,0 +1,28 @@
+package org.lkop.project2ndchance;
+
+public class AwardData {
+    
+    private int id;
+    private int prize;
+    
+    
+    public void setId(int id){
+    
+        this.id = id;
+    }
+    
+    public int getId(){
+    
+        return this.id;
+    }
+    
+    public void setPrize(int prize){
+    
+        this.prize = prize;
+    }
+    
+    public int getPrize(){
+    
+        return this.prize;
+    }
+}
