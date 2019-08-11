5b1fb0ddfa8ae8832de691c4b97620ea3a5c6aa6
Lkop <37883287+Lkop@users.noreply.github.com>
2019-08-11 03:04:06 +0300

    Adding date and phase methods

--- src/main/java/org/lkop/project2ndchance/StringCheck.java
+++ src/main/java/org/lkop/project2ndchance/StringCheck.java
@@ -1,5 +1,8 @@
 package org.lkop.project2ndchance;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class StringCheck {
     
     //proposal methods
@@ -42,20 +45,7 @@ public class StringCheck {
         }
         return 0;
     }
-    
-    public boolean getPhasePass(String string){
-        
-        String[] str_split = string.split(": ");
-        
-        if(str_split[0].equalsIgnoreCase("phase pass")){
-            
-            if(str_split[1].equalsIgnoreCase("yes")){
-                return true;
-            }
-        }
-        return false;
-    }
-    
+
     //awards methods
     public int getId(String string){
     
@@ -76,4 +66,58 @@ public class StringCheck {
         }
         return 0;
     }
+    
+    //information methods
+    public boolean getPhasePass(String string){
+        
+        String[] str_split = string.split(": ");
+        
+        if(str_split[0].equalsIgnoreCase("phase pass")){
+
+            if(str_split[1].equalsIgnoreCase("yes")){        
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean getFinalPass(String string){
+        
+        String[] str_split = string.split(": ");
+        
+        if(str_split[0].equalsIgnoreCase("project done")){
+            
+            if(str_split[1].equalsIgnoreCase("yes")){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public LocalDate getStartDate(String string){
+        
+        String[] str_split = string.split(": ");
+        
+        if(str_split[0].equalsIgnoreCase("start date")){
+            
+            if(!str_split[1].isEmpty()){
+                return LocalDate.parse(str_split[1], DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            }
+        }
+        return null;
+    }
+    
+    public LocalDate getEndDate(String string){
+        
+        String[] str_split = string.split(": ");
+        
+        if(str_split[0].equalsIgnoreCase("end date")){
+
+            if(!str_split[1].isEmpty()){
+                return LocalDate.parse(str_split[1], DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            }
+        }
+        return null;
+    }
+    
 }
